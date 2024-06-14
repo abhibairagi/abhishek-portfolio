90205d0e7ab40c8eaf761ae5adeb154b40ca9d23
fahad065 <40202393+fahad065@users.noreply.github.com>
2024-06-14 10:10:44 +0400

    Update Type.jsx

--- src/Components/About/Type.jsx
+++ src/Components/About/Type.jsx
@@ -5,7 +5,7 @@ import Typewriter from "typewriter-effect";
 export const Type = () => {
     return (
         <>
-            <Typewriter options={{ strings: ["Full Stack Web Developer", "MEVN/MEAN/MERN Stack Developer", "Chess Enthusiast", "YouTuber", "Blogger"], autoStart: true, loop: true, deleteSpeed: 50 }}
+            <Typewriter options={{ strings: ["Full Stack Web Developer", "MEVN/MEAN/MERN Stack Developer", "Chess Enthusiast", "Blogger"], autoStart: true, loop: true, deleteSpeed: 50 }}
             />
         </>
     );
